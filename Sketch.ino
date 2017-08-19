157a233d13d5745c86c7351be7da02c0a20be905
Niklas Mischkulnig <niklas@mischkulnig.de>
2017-08-19 21:24:06 +0200

    Update for new library

--- Sketch.ino
+++ Sketch.ino
@@ -1,9 +1,7 @@
-#include "Arduino.h"
 #include "Sketch.h"
-
+#include "SoftwareSerial.h"
 
 int count = 0;
-
 
 void getState(Data *d, int id){
 	if(id & DATA_PIN){
@@ -11,43 +9,46 @@ void getState(Data *d, int id){
 			strcpy(d->desc, "BLINKY");
 		}
 	} else if(id & DATA_INFO){
-		if((id & DATA_MASK) == 0){
-			// strcpy(d->desc, "VCC");
+		int i = 0;
+		if((id & DATA_MASK) == i++){
+			strcpy(d->desc, "VCC");
 			d->vtype = Data::afloat;
 			d->f = 5.0f;
-		} else if((id & DATA_MASK) == 1){
+		} else if((id & DATA_MASK) == i++){
 			strcpy(d->desc, "counter");
 			d->vtype = Data::aint;
 			d->i = count;
-		} else if((id & DATA_MASK) == 2){
+		} else if((id & DATA_MASK) == i++){
 			strcpy(d->desc, "counterEven");
 			d->vtype = Data::abool;
-			d->i = count%2 == 0;
-		} else if((id & DATA_MASK) == 3){
-			strcpy(d->desc, "cDec");
+			d->b = count%2 == 0;
+		} else if((id & DATA_MASK) == i++){
+			strcpy(d->desc, "counterDec");
 			d->vtype = Data::afloat;
 			d->f = count/10.0f;
-		}  else if((id & DATA_MASK) == 4){
-			strcpy(d->desc, "MOTOR");
-			d->vtype = Data::aint;
-			int dir = count%3; // 0:none, 1:counterclockwise, 2:clockwise
-			int state = (count/3)%3; // 0:white, 1:red, 2:green
-			d->i = (dir << 4) | (state);
+		} else if((id & DATA_MASK) == i++){
+			d->vtype = Data::aMotor;
+			// MotorState::NONE ::LEFT ::RIGHT
+			d->ms.dir = MotorState::Dirs(count%3);
+			// MotorState::RED ::GREEN ::WHITE
+			d->ms.state =  MotorState::States((count/3)%3);
 		}
 	}
 }
 
-
+SoftwareSerial Serial(6, 7);
 
 void setup() {
+	Serial.begin(9600);
+	Serial.println("Setup called");
 	pinMode(1, OUTPUT);
 }
 
-
 void loop() {
+	Serial.println("Loop");
 	digitalWrite(1, HIGH);
-	delay(1000);
+	delay(500);
 	digitalWrite(1, LOW);
-	delay(1000);
+	delay(500);
 	count++;
 }
